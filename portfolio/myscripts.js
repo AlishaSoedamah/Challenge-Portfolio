e878e2fb71f06614b524c1e9a257edb7b25238a2
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2021-01-15 10:25:23 +0100

    nothing changed I just am scared

--- portfolio/myscripts.js
+++ portfolio/myscripts.js
@@ -1,7 +1,14 @@
 let bezoeker = {
-    voornaam : prompt("Wat is je voornaam?"),
+    naam : prompt("Wat is je voornaam?"),
     leeftijd : prompt("Wat is je leeftijd?")
 };
 
-alert("Welkom"+ " " +bezoeker.voornaam + "!")
+alert("Welkom"+" "+bezoeker["naam"] +"!");
+console.log(bezoeker);
 
+let voornaamElement = document.querySelector('#naam');
+naamElement.innerHTML = bezoeker.naam;
+//BGcolours
+if (bezoeker["leeftijd"]=18) {
+    document.querySelector("body").style.backgroundColor = "lightgreen";
+}
