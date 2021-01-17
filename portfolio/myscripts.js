97e33011c8dbac37f64a018d4bdf3fe52863c697
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2021-01-17 18:32:30 +0100

    Input naam getoond op index pagina

--- portfolio/myscripts.js
+++ portfolio/myscripts.js
@@ -2,13 +2,19 @@ let bezoeker = {
     naam : prompt("Wat is je voornaam?"),
     leeftijd : prompt("Wat is je leeftijd?")
 };
-
 alert("Welkom"+" "+bezoeker["naam"] +"!");
+
+//object bezoeker in consolelog
 console.log(bezoeker);
 
-let voornaamElement = document.querySelector('#naam');
+//naam tonen op pagina
+let naamElement = document.querySelector('#naam');
 naamElement.innerHTML = bezoeker.naam;
+
 //BGcolours
 if (bezoeker["leeftijd"]=18) {
     document.querySelector("body").style.backgroundColor = "lightgreen";
+}
+if (bezoeker["leeftijd"]>18) {
+    document.querySelector("body").style.backgroundColor = "red";
 }
